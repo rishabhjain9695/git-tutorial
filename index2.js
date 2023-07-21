d5693f05f0b0b7c4cee57fef9ebd34b8e8132339
rishabh jain <goyal.rishabh12@gmail.com>
2023-07-21 22:31:42 +0530

    ff

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ import React from 'react'
 function index2() {
   return (
     <div>
-      
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus et consectetur neque? Laborum, porro? Voluptas repudiandae, exercitationem molestiae eius voluptate autem, accusantium, est corrupti animi sunt aliquam fugiat in earum.
     </div>
   )
 }
